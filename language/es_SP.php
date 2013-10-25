f802609bc04062400712d34fa384ecbf5d6eea04
coralienoulette <coralie.noulette@adfab.fr>
2013-10-25 17:51:07 +0200

    PLAYGROUND-134: Trad module PARTNERS

---
+++ language/es_SP.php
@@ -0,0 +1,46 @@
+<?php
+return array(
+
+    // Breadcrumbs
+    'Home' => 'Home',
+
+    // Admin
+    'Admin' => 'Administration',
+    'Dashboard' => 'Dashboard',
+    'New Action' => 'New Action',
+    'System' => 'System',
+    'Settings' => 'Settings',
+    'Edit FAQ' => 'Edit FAQ',
+    'Edit' => 'Edit',
+    'Delete' => 'Delete',
+    'Add new page' => 'Add new page',
+    'Create' => 'Create',
+    'title' => 'Title',
+    'identifier' => 'Identifier',
+    'is_active' => 'Is active',
+    'sort_order' => 'Sort order',
+    'Add new block' => 'Add new content block',
+    'Block' => 'Content block',
+    'Content' => 'Content',
+    'Add' => 'Add',
+    'Dynarea' => 'Dynarea',
+    'Dynablocks' => 'Dynablocks',
+    'Available Dynareas' => 'Available Dynareas',
+
+	'Partners' => 'Partners',
+	'Id' => 'Id',
+	'Name' => 'Name',
+	'Logo' => 'Logo',
+	'Small logo' => 'Small logo',
+	'Active' => 'Active',
+	'Add partner' => 'Create partner',
+		
+	// Admin create partners
+	'Yes' => 'Yes',
+	'No' => 'No',
+	'Web site' => 'Web site',
+	'Facebook Page' => 'Facebook Page',
+	'Active newsletter registration' => 'Active newsletter registration',
+	'Recirculation page' => 'Recirculation page',
+		
+);
